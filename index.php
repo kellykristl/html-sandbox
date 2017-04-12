8a84c462349a8f01e82ec18ca5a7bfa6d210c97e
kellykristl <goddesswebdesign@gmail.com>
2017-04-12 08:49:24 -0600

    added text and image

--- index.php
+++ index.php
@@ -12,6 +12,19 @@
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
+		<header>
+			<h1>HTML Sandbox</h1>
+		</header>
 
+		<main>
+			<p>Every day is taco ipsum tuesday. Side of rice and beans, please. Josh’s taco shack is the best taco shack. 50 cent tacos! I’ll take 30. Give me all the tacos, immediately. Tacos dorados called flautas, or taquitos, for which the tortillas are filled with pre-cooked shredded chicken, beef or barbacoa, rolled into an elongated cylinder and deep-fried until crisp. Flour or corn tortillas? Add in a few el Pastor with guac and diced onions. How do you feel about hard shelled tacos? CARNITAS!!
+			</p>
+			<p>Every day is taco ipsum tuesday. Side of rice and beans, please. Josh’s taco shack is the best taco shack. 50 cent tacos! I’ll take 30. Give me all the tacos, immediately. Tacos dorados called flautas, or taquitos, for which the tortillas are filled with pre-cooked shredded chicken, beef or barbacoa, rolled into an elongated cylinder and deep-fried until crisp. Flour or corn tortillas? Add in a few el Pastor with guac and diced onions. How do you feel about hard shelled tacos? CARNITAS!!
+			</p>
+			<p>
+				<img src="images/about.jpg">
+			</p>
+
+		</main>
 	</body>
 </html>
