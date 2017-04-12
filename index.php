9455b0a0ab74c89924b518974da64f09f242102d
kellykristl <goddesswebdesign@gmail.com>
2017-04-12 09:00:44 -0600

    added new link

--- index.php
+++ index.php
@@ -21,9 +21,8 @@
 			</p>
 			<p>Every day is taco ipsum tuesday. Side of rice and beans, please. Josh’s taco shack is the best taco shack. 50 cent tacos! I’ll take 30. Give me all the tacos, immediately. Tacos dorados called flautas, or taquitos, for which the tortillas are filled with pre-cooked shredded chicken, beef or barbacoa, rolled into an elongated cylinder and deep-fried until crisp. Flour or corn tortillas? Add in a few el Pastor with guac and diced onions. How do you feel about hard shelled tacos? CARNITAS!!
 			</p>
-			<p>
-				<img src="images/about.jpg">
-			</p>
+			<img src="images/about.jpg" alt="me">
+			<p>This is a link to my favorite site: <a href="http://php.net" target="_blank">PHP.net</a></p>
 
 		</main>
 	</body>
